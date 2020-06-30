22e7e4925984a0476fa1401345cc0a1310a91402
Sel007 <selina.08225.1@gmail.com>
2020-06-30 12:00:24 -0400

    Add project files.

---
+++ ClassesDemo2ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesDemo2ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
